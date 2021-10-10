994725a1f09138238a720ee6614cdae823a413cd
Adam Paruk <dadas1000@gmail.com>
2021-10-10 15:55:25 +1100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f https://dadas1000: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@github.com/dadas1000/interactive-language-reader.git master:gh-pages
-cd -
+cd /
